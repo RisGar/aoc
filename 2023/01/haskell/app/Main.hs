671d293f0a58ca51e595cae7d0e08f24c82f51cd
Rishab Garg <me@rishab-garg.me>
2023-12-26 20:08:35 +0100

    Updated Day 1 Solution in Haskell

--- 2023/01/haskell/app/Main.hs
+++ 2023/01/haskell/app/Main.hs
@@ -1,43 +1,44 @@
 module Main where
 
 import Data.Char (digitToInt, isDigit)
-import Data.List (tails, isPrefixOf)
+import Data.List (isPrefixOf, tails)
 import Data.Maybe (mapMaybe)
 
 parseDigits :: String -> [Int]
-parseDigits xs = map digitToInt $ filter isDigit xs
+parseDigits = map digitToInt . filter isDigit
 
-parseDigits' :: [String] -> [Int]
-parseDigits' xs = mapMaybe parseDigit' xs
+parseDigits' :: String -> [Int]
+parseDigits' = mapMaybe parseDigit' . tails
 
 parseDigit' :: String -> Maybe Int
 parseDigit' [] = Nothing
-parseDigit' xxs@(x:_)
-    | isPrefixOf "one" xxs = Just 1
-    | isPrefixOf "two" xxs = Just 2
-    | isPrefixOf "three" xxs = Just 3
-    | isPrefixOf "four" xxs = Just 4
-    | isPrefixOf "five" xxs = Just 5
-    | isPrefixOf "six" xxs = Just 6
-    | isPrefixOf "seven" xxs = Just 7
-    | isPrefixOf "eight" xxs = Just 8
-    | isPrefixOf "nine" xxs = Just 9
-    | isDigit x = Just (digitToInt x)
-    | otherwise = Nothing
+parseDigit' xxs@(x : _)
+  | f "one" = Just 1
+  | f "two" = Just 2
+  | f "three" = Just 3
+  | f "four" = Just 4
+  | f "five" = Just 5
+  | f "six" = Just 6
+  | f "seven" = Just 7
+  | f "eight" = Just 8
+  | f "nine" = Just 9
+  | isDigit x = Just (digitToInt x)
+  | otherwise = Nothing
+  where
+    f = flip isPrefixOf xxs
 
 headPlusTail :: [Int] -> Int
 headPlusTail [] = error "Empty list"
-headPlusTail xxs@(x:_) = x * 10 + last xxs
+headPlusTail xxs@(x : _) = x * 10 + last xxs
 
 part1 :: [String] -> String
-part1 xs = show . sum $ headPlusTail <$> map parseDigits xs
+part1 = show . sum . map (headPlusTail . parseDigits)
 
 part2 :: [String] -> String
-part2 xs = show . sum $ headPlusTail <$> map parseDigits' (map tails xs)
-
+part2 = show . sum . map (headPlusTail . parseDigits')
 
 main :: IO ()
 main = do
   input <- readFile "../input/input.txt"
-  print ("Part 1: " ++ (part1 $ lines input))
-  print ("Part 2: " ++ (part2 $ lines input))
+  print $ "Part 1: " ++ (part1 . lines) input
+  print $ "Part 2: " ++ (part2 . lines) input
